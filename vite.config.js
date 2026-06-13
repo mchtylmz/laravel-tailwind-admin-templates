79c6d03103d2937dea37e8f57019bf16a531f44b
Mücahit Yılmaz <mucahit.yilmaz@mivento.com>
2026-06-13 20:27:09 +0300

    vite docker icin host ve origin ayari

--- vite.config.js
+++ vite.config.js
@@ -17,6 +17,9 @@ export default defineConfig({
         tailwindcss(),
     ],
     server: {
+        host: '0.0.0.0',
+        port: 5173,
+        origin: 'http://localhost:5173',
         watch: {
             ignored: ['**/storage/framework/views/**'],
         },
